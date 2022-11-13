bed39d6680e7d63b381e248e6c3262c232138bd3
yvonne0414 <yvonne9456@gmail.com>
2022-11-13 17:59:01 +0800

    add 購物車 元件渲染

--- src/Pages/ShopCart/index.js
+++ src/Pages/ShopCart/index.js
@@ -1,11 +1,32 @@
 import Step from '../../components/Step';
 import ShoppingBasket from '../../components/ShoppingBasket';
+import { useState } from 'react';
+
+const initData = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  },
+]
+
 const ShopCart = () => {
+  const [sum, setSum] = useState(0);
+  const [cartPrdList, setCartPrdList] = useState(initData);
   return (
     <main class="site-main">
       <div class="main-container">
         <Step/>
-        <ShoppingBasket/>
+        <ShoppingBasket cartPrdList={cartPrdList} sum={sum} />
       </div>
     </main>
   )
--- src/components/ShoppingBasket/index.js
+++ src/components/ShoppingBasket/index.js
@@ -1,33 +1,41 @@
 import {ReactComponent as Minus} from '../../assets/icons/minus.svg' 
 import {ReactComponent as Plus} from '../../assets/icons/plus.svg' 
-import Prd1 from '../../assets/images/product-1.jpg'
-import Prd2 from '../../assets/images/product-2.jpg'
+// import Prd1 from '../../assets/images/product-1.jpg'
+// import Prd2 from '../../assets/images/product-2.jpg'
 
-const ShoppingBasket = () => {
+const ShoppingBasket = (props) => {
+  const {cartPrdList, sum} = props
+
   return (        
     <section class="cart-container col col-lg-5 col-sm-12">
           <h3 class="cart-title">購物籃</h3>
 
           <section class="product-list col col-12" data-total-price="0">
-            <div class="product-container col col-12" data-count="0" data-price="3999">
-              <img class="img-container" src={Prd1} alt="Prd2"  />
-              <div class="product-info">
-                <div class="product-name">破壞補丁修身牛仔褲</div>
-                <div class="product-control-container">
-                  <div class="product-control">
-                    <div class="product-action minus">
-                      <Minus/>
-                    </div>
-                    <span class="product-count"></span>
-                    <div class="product-action plus">
-                      <Plus/>
+            {
+              cartPrdList.map((prd)=>{
+                return (
+                  <div class="product-container col col-12" key={prd.id}>
+                    <img class="img-container" src={prd.img} alt="Prd2"  />
+                    <div class="product-info">
+                      <div class="product-name">{prd.name}</div>
+                      <div class="product-control-container">
+                        <div class="product-control">
+                          <div class="product-action minus" >
+                            <Minus/>
+                          </div>
+                          <span class="product-count">{prd.quantity}</span>
+                          <div class="product-action plus">
+                            <Plus/>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="price">$ {prd.price*prd.quantity}</div>
                     </div>
                   </div>
-                </div>
-                <div class="price"></div>
-              </div>
-            </div>
-            <div class="product-container col col-12" data-count="0" data-price="1299">
+                )
+              })
+            }
+            {/* <div class="product-container col col-12" data-count="0" data-price="1299">
               <img class="img-container" src={Prd2} alt="Prd2" />
               <div class="product-info">
                 <div class="product-name">刷色直筒牛仔褲</div>
@@ -44,12 +52,12 @@ const ShoppingBasket = () => {
                 </div>
                 <div class="price"></div>
               </div>
-            </div>
+            </div> */}
           </section>
 
           <section class="cart-info shipping col col-12">
             <div class="text">運費</div>
-            <div class="price"></div>
+            <div class="price">免費</div>
           </section>
           <section class="cart-info total col col-12">
             <div class="text">小計</div>
